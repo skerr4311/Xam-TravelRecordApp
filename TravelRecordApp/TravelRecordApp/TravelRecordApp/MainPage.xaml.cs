1bb457dc53cc765a8b913fdd7a50a8b3f281b39a
Steve <sker078@aucklanduni.ac.nz>
2021-08-20 11:46:26 +1200

    Adding logic to the button

--- TravelRecordApp/TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -17,7 +17,17 @@ namespace TravelRecordApp
 
         private void LoginButton_Clicked(object sender, EventArgs e)
         {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+                //one or both entries are empty
+            }
+            else
+            {
+                //Both are full
+            }
         }
     }
 }
